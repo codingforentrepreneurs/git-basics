058f07b57ad9d57e473f7721616c925d45ae0c4a
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-01 12:42:59 -0700

    Added blog function/app

--- gbasics/urls.py
+++ gbasics/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gbasics.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
